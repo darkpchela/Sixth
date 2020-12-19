0c7d453580e8f1b2227ddac3d988a2bdccdc2dca
darkpchela <darkpchela2@gmail.com>
2020-12-19 23:41:24 +0300

    creating and loading done

--- Sixth/Services/MainHub.cs
+++ Sixth/Services/MainHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Sixth.Interfaces;
+using Sixth.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,23 @@ namespace Sixth.Services
 {
     public class MainHub : Hub
     {
+        private readonly INodeCrudService nodeCrudService;
+
+        public MainHub(INodeCrudService nodeCrudService)
+        {
+            this.nodeCrudService = nodeCrudService;
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            var nodes = await nodeCrudService.GetAll();
+            await Clients.Caller.SendAsync("OnConnected", nodes);
+        }
+
+        public async Task CreateNode(Node node)
+        {
+            var id = await nodeCrudService.Create(node);
+            await Clients.All.SendAsync("OnNodeCreated", id, node);
+        }
     }
 }
--- Sixth/Startup.cs
+++ Sixth/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sixth.Interfaces;
 using Sixth.Services;
 using System;
 using System.Collections.Generic;
@@ -22,16 +23,15 @@ namespace Sixth
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             string conString = Configuration.GetConnectionString("Default");
             services.AddControllersWithViews();
             services.AddSignalR();
             services.AddDbContext<AppDbContext>(o => o.UseSqlServer(conString));
+            services.AddScoped<INodeCrudService, NodeCrudService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +41,6 @@ namespace Sixth
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
